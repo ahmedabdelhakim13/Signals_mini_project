6650ddbb7788e0f1b85917d413814b58f370a927
ahmed abdelhakem <104411735+ahmedabdelhakim13@users.noreply.github.com>
2023-05-14 15:32:14 +0300

    sc

--- Final_Project/transimitter.m
+++ Final_Project/transimitter.m
@@ -1,19 +1,30 @@
 %reading the sound
 [y,f_s] = audioread('gamed.mp3');
 sound(y,f_s);
+
 %time representation
+
 N = length(y);
 t=linspace(0,N/f_s,N);
-subplot(2,1,1);
 plot(t,y);
 xlabel('Time');
 ylabel('sound');
 title('Time representation')
 clear sound
+
 %frequencyrepresentation
+
 Y=fftshift(fft(y));
-subplot(2,1,2);
-plot(Y);
+Y_magnitude= abs(Y);
+Y_phase = angle(Y);
+fvec=linspace(-f_s/2,f_s/2,N);
+subplot(2,1,1)
+plot(fvec,Y_magnitude);
 xlabel('Frequency');
 ylabel('sound');
-title('Frequency representation');
+title('Frequency magnitude representation');
+subplot(2,1,2)
+plot(fvec,Y_phase);
+xlabel('Frequency ');
+ylabel('sound');
+title('Frequency angle representation');
