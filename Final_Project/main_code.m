cd8f6310a7eb168ae9987a11d0dd6d3aae9de253
ahmed abdelhakem <104411735+ahmedabdelhakim13@users.noreply.github.com>
2023-05-14 16:45:12 +0300

    Update main_code.m

--- Final_Project/main_code.m
+++ Final_Project/main_code.m
@@ -1,13 +1,13 @@
 %reading the sound
-[y,f_s] = audioread('gamed.mp3');
-sound(y,f_s);
+[x,f_s] = audioread('gamed.mp3');
+sound(x,f_s);
 
 %time representation
 
-N = length(y);
+N = length(x);
 t=linspace(0,N/f_s,N);
 figure;
-plot(t,y);
+plot(t,x);
 xlabel('Time');
 ylabel('sound');
 title('Time representation')
@@ -15,18 +15,18 @@ clear sound
 
 %frequencyrepresentation
 
-Y=fftshift(fft(y));
-Y_magnitude= abs(Y);
-Y_phase = angle(Y);
+X=fftshift(fft(x));
+X_magnitude= abs(X);
+X_phase = angle(X);
 fvec=linspace(-f_s/2,f_s/2,N);
 figure;
 subplot(2,1,1)
-plot(fvec,Y_magnitude);
+plot(fvec,X_magnitude);
 xlabel('Frequency');
 ylabel('sound');
 title('Frequency magnitude representation');
 subplot(2,1,2)
-plot(fvec,Y_phase);
+plot(fvec,X_phase);
 xlabel('Frequency ');
 ylabel('sound');
 title('Frequency angle representation');
