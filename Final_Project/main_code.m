cb84921d75c47c3641505824ba6fcccd0c118ba9
ahmed abdelhakem <104411735+ahmedabdelhakim13@users.noreply.github.com>
2023-05-14 17:54:05 +0300

    v

--- Final_Project/main_code.m
+++ Final_Project/main_code.m
@@ -3,7 +3,6 @@
 sound(x,f_s);
 
 %time representation
-
 N = length(x);
 t=linspace(0,N/f_s,N);
 figure;
@@ -14,7 +13,6 @@ title('Time representation')
 clear sound
 
 %frequencyrepresentation
-
 X=fftshift(fft(x));
 X_magnitude= abs(X);
 X_phase = angle(X);
@@ -33,11 +31,41 @@ title('Frequency angle representation');
 
 %channels
 channels=input('Enter the number of the channel you want to perform on the signal : \n 1)Delta function  \n 2)exp(-2pi*5000t) \n 3)exp(-2pi*1000t)\n 4)impulse response \n');  
- 
-if channels==1
+
+% Define the impulse responses
+h1 = [1 zeros(1, N-1)]; % Delta function
+h2 = exp(-2*pi*5000*t); % exp(-2pi*5000t)
+h3 = exp(-2*pi*1000*t); % exp(-2pi*1000t)
+
+% Perform convolution based on the selected channel
+if channels==1   
+    z = x(:);
+    y = conv(z, h1');
     
 elseif channels==2
+    y = conv(x, h2');
+
 elseif channels==3
+    y = conv(x, h3);
+
 elseif channels==4
+    % Define the impulse response for channel 4
+    h4 = [1 2 3 2 1];
+    y = conv(x, h4);
 end
+
+% Plot the result
+figure;
+subplot(2,1,1);
+plot(t, x);
+title('Original sound message');
+xlabel('Time (s)');
+ylabel('Amplitude');
+
+subplot(2,1,2);
+plot(t, y(1:N));
+title('Convolved sound message');
+xlabel('Time (s)');
+ylabel('Amplitude');
+
  
