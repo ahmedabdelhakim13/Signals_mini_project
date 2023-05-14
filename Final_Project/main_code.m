1a866cb2ca1673d6743613c662820fd60b31c8cc
ahmed abdelhakem <104411735+ahmedabdelhakim13@users.noreply.github.com>
2023-05-14 18:02:57 +0300

    mm

--- Final_Project/main_code.m
+++ Final_Project/main_code.m
@@ -1,12 +1,12 @@
 %reading the sound
 [x,f_s] = audioread('gamed.mp3');
+%play the sound
 sound(x,f_s);
-
 %time representation
-N = length(x);
-t=linspace(0,N/f_s,N);
-figure;
-plot(t,x);
+N = length(x);  %length of x
+t=linspace(0,N/f_s,N); %time 
+subplot(3,1,1)
+plot(t,x); %origional sound in time
 xlabel('Time');
 ylabel('sound');
 title('Time representation')
@@ -17,25 +17,28 @@ X=fftshift(fft(x));
 X_magnitude= abs(X);
 X_phase = angle(X);
 fvec=linspace(-f_s/2,f_s/2,N);
-figure;
-subplot(2,1,1)
-plot(fvec,X_magnitude);
+subplot(3,1,2)
+plot(fvec,X_magnitude); %origional sound in frequency
 xlabel('Frequency');
 ylabel('sound');
 title('Frequency magnitude representation');
-subplot(2,1,2)
-plot(fvec,X_phase);
+subplot(3,1,3)
+plot(fvec,X_phase); %origional sound in phase
 xlabel('Frequency ');
 ylabel('sound');
 title('Frequency angle representation');
 
 %channels
-channels=input('Enter the number of the channel you want to perform on the signal : \n 1)Delta function  \n 2)exp(-2pi*5000t) \n 3)exp(-2pi*1000t)\n 4)impulse response \n');  
-
 % Define the impulse responses
 h1 = [1 zeros(1, N-1)]; % Delta function
 h2 = exp(-2*pi*5000*t); % exp(-2pi*5000t)
 h3 = exp(-2*pi*1000*t); % exp(-2pi*1000t)
+%h4=
+%implementation of the channels
+
+channels=input('Enter the number of the channel you want to perform on the signal : \n 1)Delta function  \n 2)exp(-2pi*5000t) \n 3)exp(-2pi*1000t)\n 4)impulse response \n');  
+
+
 
 % Perform convolution based on the selected channel
 if channels==1   
